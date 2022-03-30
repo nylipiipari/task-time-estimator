bfa0d6433f754374ad659e842e1b4ce77c15b004
Nick Yli-Piipari <nylipiipari@gmail.com>
2022-03-30 17:27:34 -0400

    removed temp code from client App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,33 +2,17 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [items, setItems] = useState(null);
 
   useEffect(() => {
     fetch('/items')
       .then(res => res.json())
-      .then(data => setData(data.message));
+      .then(data => setItems(data.message));
   }, []);
-
-  // useEffect(() => {
-  //   fetch('/api', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       field1: 'Yooooo',
-  //       field2: 'Its ya boy!',
-  //     })
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => console.log(data));
-  // }, []);
 
   return (
     <div className="App">
       <h1>Task Time Estimator</h1>
-      <p>{!data ? 'Loading...' : data}</p>
     </div>
   );
 }
